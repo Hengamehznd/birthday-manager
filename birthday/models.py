a8e3d280ddbd39c5fbac53efbfbe2469f8521a6e
Hengamehznd <162028907+Hengamehznd@users.noreply.github.com>
2026-09-13 15:16:26 +0330

    dorostt shod?!

--- birthday/models.py
+++ birthday/models.py
@@ -1,14 +1,46 @@
+from datetime import date
+
 from django.db import models
 
 
 class Birthday(models.Model):
     class Relation(models.TextChoices):
-        Friend = "FRIEND", "دوست"
-        # Friend = 'FRIEND', 'دوست'
+        FRIEND = "FRIEND", "دوست"
+        FAMILY = "FAMILY", "خانواده"
+        COLLEAGUE = "COLLEAGUE", "همکار"
+        OTHER = "OTHER", "سایر"
+        #ok
 
-    name = models.CharField(max_length=250)
-    relation = models.CharField(max_length=10, choices=Relation.choices)
-    birthday_date = models.DateField()
+    name = models.CharField(max_length=250, verbose_name="نام و نام خانوادگی")
+    relation = models.CharField(
+        max_length=10,
+        choices=Relation.choices,
+        verbose_name="نسبت",
+    )
+    birthday_date = models.DateField(verbose_name="تاریخ تولد")
 
     class Meta:
-        ordering = ["-birthday_date"]
+        ordering = ["birthday_date"]
+        verbose_name = "تولد"
+        verbose_name_plural = "تولدها"
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def age(self):
+        today = date.today()
+        return today.year - self.birthday_date.year - (
+            (today.month, today.day)
+            < (self.birthday_date.month, self.birthday_date.day)
+        )
+
+    @property
+    def days_until_birthday(self):
+        today = date.today()
+        next_birthday = self.birthday_date.replace(year=today.year)
+
+        if next_birthday < today:
+            next_birthday = self.birthday_date.replace(year=today.year + 1)
+
+        return (next_birthday - today).days
