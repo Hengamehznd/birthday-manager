d87ebc92379992daa8eb0e43fc6e6198b4c0a18a
Hengamehznd <162028907+Hengamehznd@users.noreply.github.com>
2026-09-13 15:22:47 +0330

    i edited models.py base.html ,home,navbar and created forms.py

---
+++ birthday/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+from .models import Birthday
+
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = Birthday
+        fields = ["name", "birthday_date", "relation"]
+        labels = {
+            "name": "نام و نام خانوادگی",
+            "birthday_date": "تاریخ تولد",
+            "relation": "نسبت",
+        }
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "نام و نام خانوادگی",
+                    "required": True,
+                }
+            ),
+            "birthday_date": forms.DateInput(
+                attrs={
+                    "class": "form-control",
+                    "type": "date",
+                    "required": True,
+                }
+            ),
+            "relation": forms.Select(
+                attrs={
+                    "class": "form-select",
+                }
+            ),
+        }
